13b50f8c0c0d54ae7167fbd341a5124d9867c45d
wugang <544393709@qq.com>
2019-01-28 21:08:01 +0800

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+k
+;app/src/main/java/com/wu/library/wusimple/MainActivity.java,d/6/d648c66eb274c82d0dd9af03c6e824bb3612343c
+p
+@app/src/main/java/com/wu/library/wusimple/gift/GiftActivity.java,0/a/0ae470ef3f5bbdd7729badb4d59323ea5ca7abce
---
+++ app/src/main/java/com/wu/library/wusimple/gift/GiftActivity.java
@@ -0,0 +1,23 @@
+package com.wu.library.wusimple.gift;
+
+
+import com.wu.library.wusimple.R;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created bywugang on 2019/1/15.
+ * Describe
+ */
+public class GiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gift);
+
+    }
+}
